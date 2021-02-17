8cb2074bffc19746441b74920564c6b6f7b555c8
unknown <User@PRINT-1912.print2.local>
2021-02-17 13:23:12 +0100

    commit1.2

--- Scripts/VendorsDiffBackup.sql
+++ Scripts/VendorsDiffBackup.sql
@@ -1,8 +1,8 @@
 ---  .Scripts\VendorsDiffBackup.sql ---
-BACKUP DATABASE [Vendors] TO  DISK = N'C:\VendorDB\Backup\VendorsDiffBackup.sql' WITH  DIFFERENTIAL , NOFORMAT, NOINIT,  NAME = N'Vendors-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10, CHECKSUM
+BACKUP DATABASE [Vendors] TO  DISK = N'C:\VendorDB\Backup\VendorDiffBackup.bak' WITH  DIFFERENTIAL , NOFORMAT, NOINIT,  NAME = N'Vendors-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10, CHECKSUM
 GO
 declare @backupSetId as int
 select @backupSetId = position from msdb..backupset where database_name=N'Vendors' and backup_set_id=(select max(backup_set_id) from msdb..backupset where database_name=N'Vendors' )
 if @backupSetId is null begin raiserror(N'Verify failed. Backup information for database ''Vendors'' not found.', 16, 1) end
-RESTORE VERIFYONLY FROM  DISK = N'C:\VendorDB\Backup\VendorsDiffBackup.sql' WITH  FILE = @backupSetId,  NOUNLOAD,  NOREWIND
+RESTORE VERIFYONLY FROM  DISK = N'C:\VendorDB\Backup\VendorDiffBackup.bak' WITH  FILE = @backupSetId,  NOUNLOAD,  NOREWIND
 GO
--- Scripts/VendorsFullBackup.sql
+++ Scripts/VendorsFullBackup.sql
@@ -1,6 +1,6 @@
 -- 8. feladat
 ---  .Scripts\VendorsFullBackup.sql ---
-BACKUP DATABASE [Vendors] TO  DISK = N'C:\VendorDB\Backup\VendorFullBackup.bak' WITH NOFORMAT, NOINIT,  NAME = N'Vendors-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10, CHECKSUM
+BACKUP DATABASE [Vendors] TO  DISK = N'C:\VendorDB\Backup\VendorFullBackup.bak' WITH NOFORMAT, INIT,  NAME = N'Vendors-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10, CHECKSUM
 GO
 declare @backupSetId as int
 select @backupSetId = position from msdb..backupset where database_name=N'Vendors' and backup_set_id=(select max(backup_set_id) from msdb..backupset where database_name=N'Vendors' )
